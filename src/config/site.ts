b31e2e407c4aa45f6750fb63abdc55780f17b956
HISHAN03 <moidinhishan@gmail.com>
2025-03-18 23:46:47 +0530

    first commit

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "leaflens",
+  url: "https://leaflens.vercel.app",
+  ogImage: "https://leaflens.vercel.app/og.jpg",
+  description:
+    "Your digital gateway to the botanical world. Unearth a wealth of insights, from species details to disease detection, effortlessly bridging the gap between technology and the beauty of nature.",
+  links: {
+    twitter: "https://twitter.com/ShettyDeveesh",
+    github: "https://github.com/Deveesh-Shetty/leaflens",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
